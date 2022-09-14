e552a3ff963cdd5784fde0e0d60a2fbcf18ba21c
Vlad Shestavetskyi <vladislavshsh@gmail.com>
2022-09-14 22:15:12 +0300

    styles

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,11 +1,12 @@
 import Section from "components/Section/sections";
 import Notification from "components/Notification/Notification";
+import styles from "components/Statistics/statistics.module.css"
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
   <Section title="Statistics">
     {!total ? (
       <Notification message="There is no feedback" />
     ) : (
-      <ul>
+      <ul className={styles.list}>
         <li>Good:{good}</li>
         <li>Neutral:{neutral}</li>
         <li>Bad:{bad}</li>
